0136172665f3cf2f026256c5afbfc508b2da5ac8
Tair Olegovich <gidrolaz.tr@gmail.com>
2025-04-18 20:13:58 +0700

    Сплайны: конструктор и определение интервалов.

--- lab4/lab4/Spline.cs
+++ lab4/lab4/Spline.cs
@@ -8,5 +8,46 @@ namespace lab4
 {
     internal class Spline
     {
+        private double[] a;
+        private double[] b;
+        private double[] c;
+        private double[] d;
+
+        private int n;
+        private double[] x;
+        private double[] y;
+
+        private double[] h;
+
+        public Spline(double[] x, double[] y)
+        {
+            if (x.Length != y.Length)
+            {
+                throw new ArgumentException("Опа! Ошибка: количество точек не равное");
+            }
+
+            n = x.Length;
+
+            a = new double[n - 1];
+            b = new double[n - 1];
+            c = new double[n - 1];
+            d = new double[n - 1];
+
+            h = new double[n - 1];
+
+            SetBins();
+
+        }
+
+
+        
+
+        private void SetBins()
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                h[i] = x[i + 1] - x[i];
+            }
+        }
     }
 }
