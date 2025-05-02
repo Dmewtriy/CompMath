f75c0dd7604833d219ce1efbd3f991d367c756ad
Dmewtriy <dankin.dm@mail.ru>
2025-05-02 09:22:51 +0700

    5 lab

---
+++ lab5/lab5/Methods.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    internal class Methods
+    {
+        public delegate double Function(double x);
+
+        private readonly Function f;
+        private double h;
+
+        private double[] fValues;
+        private int n;
+
+        public Methods(Function function, double a = 3, double b = 13, int n = 550)
+        {
+            f = function;
+            this.n = n;
+
+            fValues = new double[n + 1];
+
+            h = (b - a) / (double)n;
+
+            for (int i = 0; i <= n; i++)
+            {
+                fValues[i] = f.Invoke(a + i * h);
+            }
+        }
+
+
+        private void FindStep()
+        {
+
+        }
+
+        public double RightRectangle()
+        {
+            double result = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                result += h * fValues[i];
+            }
+
+            return result;
+        }
+
+        public double Trapezoid()
+        {
+            double result = 0;
+
+            for (int i = 1; i <= n - 1; i++)
+            {
+                result += 2 * fValues[i];
+            }
+
+            return h * 0.5f * (result + fValues[0] + fValues[fValues.Length - 1]);
+        }
+
+        public double Simpson()
+        {
+            double result = 0;
+
+            for (int i = 1; i <= n - 1; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    result += 2 * fValues[i];
+
+                }
+                else
+                {
+                    result += 4 * fValues[i];
+
+                }
+            }
+
+            return h / 3.0f * (fValues[0] + result + fValues[fValues.Length - 1]);
+        }
+
+
+        public override string ToString()
+        {
+            return $"Метод Правых прямоугольников: {RightRectangle()}\n" +
+                $"Метод Трапеций: {Trapezoid()}\n" +
+                $"Метод Симпсона: {Simpson()}";
+        }
+
+    }
+}
---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods(x => (2.5f * x * x - 0.1f) / (Math.Log(x) + 1));
+            Console.WriteLine(methods.ToString());
+        }
+    }
+}
